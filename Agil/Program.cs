74d7537d0513dd2e08008b42d0172aaa42acd4f4
Agustina Disiot <70355965+agustinadisiot@users.noreply.github.com>
2022-04-28 09:32:19 -0300

    Add files via upload

---
+++ Agil/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Agil
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
